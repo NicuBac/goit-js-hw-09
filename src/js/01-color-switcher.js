e40eb9db0a9149fea5674ea555dde5390c59741e
NicuBac <baciu.nicolae1998@gmail.com>
2024-02-10 19:14:12 +0200

    First exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let intervalId;
+
+document.body.addEventListener('click', handleButtonClick);
+
+function handleButtonClick(event) {
+  const button = event.target;
+
+  if (button.tagName === 'BUTTON' && button.dataset.start !== undefined) {
+    startColorChange();
+  } else if (button.tagName === 'BUTTON' && button.dataset.stop !== undefined) {
+    stopColorChange();
+  }
+}
+
+function startColorChange() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+}
